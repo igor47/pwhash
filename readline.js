f1970282789958b61a60aff3ddf789bac5ae0971
Igor Serebryany <igor.serebryany@airbedandbreakfast.com>
2012-11-16 18:44:32 -0800

    initial functioning version

---
+++ readline.js
@@ -0,0 +1,44 @@
+// Provide a function to read from the Java
+// standard input object System.in.
+//
+// License: MIT / X11
+// Copyright (c) 2009 by James K. Lawless
+// jimbo@radiks.net http://www.radiks.net/~jimbo
+// http://www.mailsend-online.com
+//
+// Permission is hereby granted, free of charge, to any person
+// obtaining a copy of this software and associated documentation
+// files (the "Software"), to deal in the Software without
+// restriction, including without limitation the rights to use,
+// copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following
+// conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+// OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+// OTHER DEALINGS IN THE SOFTWARE.
+
+importPackage(java.io, java.lang);
+
+var stdin=null;
+
+// We can't reference System.in because "in"
+// is a JS keyword, so we use the brackets to
+// dereference the "in" member of System.
+function readLine() {
+   if(stdin==null) {
+     stdin=new BufferedReader(
+       new InputStreamReader(
+         System['in']));
+   }
+   return stdin.readLine();
+}
